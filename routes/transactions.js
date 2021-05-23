e4b4e5642c3e71ac0c50c144e7b01812fb89bb35
Felix Piskunov <joylandraver@gmail.com>
2021-05-24 00:57:06 +0300

    Added validation for create transaction route

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 const transactions = require('../services/transactions');
 
 router.get('/', (req, res) => {
@@ -8,11 +9,20 @@ router.get('/', (req, res) => {
   });
 })
 
-router.post('/', (req, res) => {
-  const {tradingParty, counterParty, amount} = req.body;
-  transactions.create(tradingParty, counterParty, amount).then(data => {
-    res.send(data)
-  }).catch(e => res.status(400).send(e.message));
+router.post('/', 
+  body('tradingParty').notEmpty().withMessage('Trading party is mandatory'), 
+  body('counterParty').notEmpty().withMessage('Counter party is mandatory'), 
+  body('amount').notEmpty().withMessage('Amount is mandatory'),
+  body('amount').not().equals("0").withMessage('Amount should not be zero'),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {tradingParty, counterParty, amount} = req.body;
+    transactions.create(tradingParty, counterParty, amount).then(data => {
+      res.send(data)
+    }).catch(e => res.status(400).send(e.message));
 })
 
 module.exports = router;
